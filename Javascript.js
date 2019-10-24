734157d7284e805a56b20816da52dce8fe647ba5
Umida Boltaeva <umidakhon.boltaeva@gmail.com>
2019-10-24 11:40:42 +1100

    creating list in JS

--- Javascript.js
+++ Javascript.js
@@ -5,3 +5,13 @@ if (mood === 'sleepy' && tirednessLevel === 6) {
 } else {
   console.log('not bed time yet');
 }
+
+var wordCount = 5;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+var myList = [["chocolate bar", 15], ["pancake", 5], ["banana", 4],  ["pens", 10], ["cabbage", 2], ["mugs", 3]];
